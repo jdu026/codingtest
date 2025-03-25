693e3a99276391d6e08158ac1943e24cce1e3e2f
jdu026 <165145499+jdu026@users.noreply.github.com>
2025-03-25 19:46:29 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        for(int i=0; i<answer.length; i++){     //입력 배열과 같은 크기의 새로운 배열 선언.
+            answer[i] += num_list[num_list.length -i -1];   //i는 정방향 인덱스, num_list.length -i -1은 역방향 인덱스(answer[i]에 num_list의 역순 요소를 복사)함
+        }
+        return answer;
+    }
+}
